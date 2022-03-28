7a1cb5f49564d2c55aa8d43ca1d767ef7ee622e0
Lucas <Lhgomez@tigre.gob.ar>
2022-03-28 10:13:37 -0300

    Inserts para base de datos, modificacion query y adicion modelo base en excel

---
+++ queryInserDatos.sql
@@ -0,0 +1,15 @@
+insert into Editoriales(nombre)
+values ('alfaguara'),('amazonia editorial'),('anfora'),('acantilado')
+
+insert into Editoriales(nombre)
+values('avon'),('createspace'),('crown publishers'),('delbolsillo'),('del nuevo extremo'),('dolmen'),('doubleday books'),('ediciones b'),('ediciones nace'),('editorial sudamericana'),('el ateneo'),('feedbooks'),('grijalbos'),('harper'),('harper paperbacks'),('harper collins'),('indiana university'),('john wiley & sons'),('mestas ediciones'),('nal'),('planeta'),('plaza & janes'),('pomaire'),('raincoast books'),('rba'),('scribner'),('sergio adrian martin'),('severed press'),('signet classics'),('sudamericana'),('the university of chicago'),('umbriel'),('university press of mississipi'),('vergara editorial'),('wiley-blackwell')
+
+
+insert into autores(Nombre, Apellido)
+values ('Arslanian','Tamar'),('Bachman','Richard'),('Balmaceda','Daniel'),('Bourdain','Antony'),('Bulat','Tomas'),('Carroll','Lewis'),('Cline','Ernest'),('Cohat','Yves'),('Coma','Oriol Comas i'),('Eisenhart','Margaret'),('Finkel','Elizabeth'),('King','Stephen'),('Larsonn','S. J.'),('McClure','Shaun'),('Mechner','Jordan'),('Orwell','George'),('Perez Rodriguez','Uxio'),('Saint-Exupery','Antoine de'),('Schereier','Jason'),('Silberchatz','Abraham'),('Sole','Alessandro Del'),('Stevenson','Robert Louis'),('Toussaint-Samat','Maguelonne'),('Verdon','Jean'),('Verne','Jules'),('Walford','Robert'),('Wilde','Oscar'),('Zonderman','Jon'),('Zweig','Stefan')
+
+insert into IDIOMAS(Idioma)
+values('Español'),('Ingles')
+
+insert into generos(CATEGORIA)
+values ('Ciencia Ficcion'),('Suspenso'),('Nanopunk'),('Thriller'),('Terror'),('')
